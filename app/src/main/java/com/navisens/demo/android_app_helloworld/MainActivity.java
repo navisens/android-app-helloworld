e7b227ad4eb9cb9b69d33a2287074fec8cfaacd4
James Grantham <james.grantham@navisens.com>
2018-08-29 12:15:57 -0700

    added descriptive comments to code

--- app/src/main/java/com/navisens/demo/android_app_helloworld/MainActivity.java
+++ app/src/main/java/com/navisens/demo/android_app_helloworld/MainActivity.java
@@ -18,6 +18,12 @@ import java.util.Map;
 
 import static android.os.SystemClock.elapsedRealtime;
 
+/*
+ * For complete documentation on Navisens SDK API
+ * Please go to the following link:
+ * https://github.com/navisens/NaviDocs/blob/master/API.Android.md
+ */
+
 public class MainActivity extends AppCompatActivity implements MotionDnaInterface{
 
     MotionDnaApplication motionDnaApplication;
@@ -39,19 +45,73 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
         String devKey = "<ENTER YOUR DEV KEY HERE>";
 
         motionDnaApplication = new MotionDnaApplication(this);
+
+        //    This functions starts up the SDK. You must pass in a valid developer's key in order for
+        //    the SDK to function. IF the key has expired or there are other errors, you may receive
+        //    those errors through the reportError() callback route.
+
         motionDnaApplication.runMotionDna(devKey);
+
+        //    Use our internal algorithm to automatically compute your location and heading by fusing
+        //    inertial estimation with global location information. This is designed for outdoor use and
+        //    will not compute a position when indoors. Solving location requires the user to be walking
+        //    outdoors. Depending on the quality of the global location, this may only require as little
+        //    as 10 meters of walking outdoors.
+
+        motionDnaApplication.setLocationNavisens();
+
+        //   Set accuracy for GPS positioning, states :HIGH/LOW_ACCURACY/OFF, OFF consumes
+        //   the least battery.
+
         motionDnaApplication.setExternalPositioningState(MotionDna.ExternalPositioningState.LOW_ACCURACY);
-//        motionDnaApplication.setLocationAndHeadingGPSMag();
+
+        //    Manually sets the global latitude, longitude, and heading. This enables receiving a
+        //    latitude and longitude instead of cartesian coordinates. Use this if you have other
+        //    sources of information (for example, user-defined address), and need readings more
+        //    accurate than GPS can provide.
 //        motionDnaApplication.setLocationLatitudeLongitudeAndHeadingInDegrees(37.787582, -122.396627, 0);
 
-        motionDnaApplication.setPowerMode(MotionDna.PowerConsumptionMode.LOW_CONSUMPTION);
-        motionDnaApplication.startUDPHostAndPort("45.79.101.164", "6512");
+        //    Set the power consumption mode to trade off accuracy of predictions for power saving.
+
+        motionDnaApplication.setPowerMode(MotionDna.PowerConsumptionMode.PERFORMANCE);
+
+        //    Connect to your own server and specify a room. Any other device connected to the same room
+        //    and also under the same developer will receive any udp packets this device sends.
+
+        motionDnaApplication.startUDP();
+
+        //    Allow our SDK to record data and use it to enhance our estimation system.
+        //    Send this file to support@navisens.com if you have any issues with the estimation
+        //    that you would like to have us analyze.
+
         motionDnaApplication.setBinaryFileLoggingEnabled(true);
+
+        //    Tell our SDK how often to provide estimation results. Note that there is a limit on how
+        //    fast our SDK can provide results, but usually setting a slower update rate improves results.
+        //    Setting the rate to 0ms will output estimation results at our maximum rate.
+
         motionDnaApplication.setCallbackUpdateRateInMs(500);
+
+        //    When setLocationNavisens is enabled and setBackpropagationEnabled is called, once Navisens
+        //    has initialized you will not only get the current position, but also a set of latitude
+        //    longitude coordinates which lead back to the start position (where the SDK/App was started).
+        //    This is useful to determine which building and even where inside a building the
+        //    person started, or where the person exited a vehicle (e.g. the vehicle parking spot or the
+        //    location of a drop-off).
         motionDnaApplication.setBackpropagationEnabled(true);
-        motionDnaApplication.setARModeEnabled(true);
-        motionDnaApplication.setARModeEnabled(false);
+
+        //    If the user wants to see everything that happened before Navisens found an initial
+        //    position, he can adjust the amount of the trajectory to see before the initial
+        //    position was set automatically.
+        motionDnaApplication.setBackpropagationBufferSize(2000);
+
+    //    Enables AR mode. AR mode publishes orientation quaternion at a higher rate.
+
+//        motionDnaApplication.setARModeEnabled(true);
     }
+
+    //    This event receives the estimation results using a MotionDna object.
+    //    Check out the Getters section to learn how to read data out of this object.
 
     @Override
     public void receiveMotionDna(MotionDna motionDna)
@@ -73,13 +133,17 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
         });
     }
 
+    //    This event receives estimation results from other devices in the server room. In order
+    //    to receive anything, make sure you call startUDP to connect to a room. Again, it provides
+    //    access to a MotionDna object, which can be unpacked the same way as above.
+    //
+    //
+    //    If you aren't receiving anything, then the room may be full, or there may be an error in
+    //    your connection. See the reportError event below for more information.
+
     @Override
     public void receiveNetworkData(MotionDna motionDna) {
-        // This method will be called by the MotionDna core
-        // if network sharing is enabled with the startUDP()
-        // or related method calls you will get location
-        // information for other devices using the same key
-        // with network sharing enabled
+
         networkUsers.put(motionDna.getID(),motionDna);
         double timeSinceBootSeconds = elapsedRealtime() / 1000.0;
         networkUsersTimestamps.put(motionDna.getID(),timeSinceBootSeconds);
@@ -106,10 +170,15 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
         networkTextView.setText(activeNetworkUsersStringBuilder.toString());
     }
 
+    //    This event receives arbitrary data from the server room. You must have
+    //    called startUDP already to connect to the room.
+
     @Override
     public void receiveNetworkData(MotionDna.NetworkCode networkCode, Map<String, ?> map) {
 
     }
+
+    //    Report any errors of the estimation or internal SDK
 
     @Override
     public void reportError(MotionDna.ErrorCode errorCode, String s) {
@@ -132,9 +201,13 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
         }
     }
 
+    //    The two required methods shown below bind
+    //    the interface to your application's activity,
+    //    so MotionDna is able to retrieve the necessary
+    //    permissions and capabilities
     @Override
     public PackageManager getPkgManager() {
-        return getPkgManager();
+        return getPackageManager();
     }
 
     @Override
