7c8f4a551013e7371d9133342c0e35cd78209e3f
James Grantham <james@navisens.com>
2020-09-25 20:37:17 -0700

    update for 2.0.0

--- app/src/main/java/com/navisens/demo/android_app_helloworld/MainActivity.java
+++ app/src/main/java/com/navisens/demo/android_app_helloworld/MainActivity.java
@@ -1,8 +1,4 @@
 package com.navisens.demo.android_app_helloworld;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,30 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.navisens.motiondnaapi.MotionDna;
-import com.navisens.motiondnaapi.MotionDnaInterface;
 import com.navisens.motiondnaapi.MotionDnaSDK;
+import com.navisens.motiondnaapi.MotionDnaSDKListener;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
 
-import static android.os.SystemClock.elapsedRealtime;
-
 /*
- * For complete documentation on Navisens SDK API
+ * For complete documentation on the MotionDnaSDK API
  * Please go to the following link:
  * https://github.com/navisens/NaviDocs/blob/master/API.Android.md
  */
 
-public class MainActivity extends AppCompatActivity implements MotionDnaInterface{
+public class MainActivity extends AppCompatActivity implements MotionDnaSDKListener {
 
     MotionDnaSDK motionDnaSDK;
-    Hashtable<String, MotionDna> networkUsers = new Hashtable<String, MotionDna>();
-    Hashtable<String, Double> networkUsersTimestamps = new Hashtable<String, Double>();
     TextView textView;
-    TextView networkTextView;
     private static final int REQUEST_MDNA_PERMISSIONS=1;
 
     @Override
@@ -42,98 +30,28 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.HELLO);
-        networkTextView = findViewById(R.id.network);
-
         // Requests app
-        ActivityCompat.requestPermissions(this,MotionDnaSDK.needsRequestingPermissions()
+        ActivityCompat.requestPermissions(this,MotionDnaSDK.getRequiredPermissions()
                 , REQUEST_MDNA_PERMISSIONS);
     }
-
-    Intent motionDnaServiceIntent;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (MotionDnaSDK.checkMotionDnaPermissions(this)) // permissions already requested
         {
-
-            // Starts a foreground service to ensure that the
-            // App continues to sample the sensors in background
-            motionDnaServiceIntent = new Intent(getAppContext(), MotionDnaForegroundService.class);
-            getAppContext().startService(motionDnaServiceIntent);
-
-            // Start the MotionDna Core
-            startMotionDna();
+            startDemo();
         }
     }
 
-    public void startMotionDna() {
-        String devKey = "<ENTER YOUR DEV KEY HERE>";
+    public void startDemo() {
+        String devKey = "<--DEVELOPER-KEY-HERE-->";
 
-        motionDnaSDK = new MotionDnaSDK(this);
+        motionDnaSDK = new MotionDnaSDK(this.getApplicationContext(),this);
 
         //    This functions starts up the SDK. You must pass in a valid developer's key in order for
         //    the SDK to function. IF the key has expired or there are other errors, you may receive
         //    those errors through the reportError() callback route.
-        HashMap<String, Object> configuration = new HashMap<>();
-        configuration.put("model","standard");
-        motionDnaSDK.run(devKey,configuration);
-
-        //    Use our internal algorithm to automatically compute your location and heading by fusing
-        //    inertial estimation with global location information. This is designed for outdoor use and
-        //    will not compute a position when indoors. Solving location requires the user to be walking
-        //    outdoors. Depending on the quality of the global location, this may only require as little
-        //    as 10 meters of walking outdoors.
-
-        motionDnaSDK.setLocationNavisens();
-
-        //   Set accuracy for GPS positioning, states :HIGH/LOW_ACCURACY/OFF, OFF consumes
-        //   the least battery.
-
-        motionDnaSDK.setExternalPositioningState(MotionDna.ExternalPositioningState.LOW_ACCURACY);
-
-        //    Manually sets the global latitude, longitude, and heading. This enables receiving a
-        //    latitude and longitude instead of cartesian coordinates. Use this if you have other
-        //    sources of information (for example, user-defined address), and need readings more
-        //    accurate than GPS can provide.
-//        motionDnaSDK.setLocationLatitudeLongitudeAndHeadingInDegrees(37.787582, -122.396627, 0);
-
-        //    Set the power consumption mode to trade off accuracy of predictions for power saving.
-
-        motionDnaSDK.setPowerMode(MotionDna.PowerConsumptionMode.PERFORMANCE);
-
-        //    Connect to your own server and specify a room. Any other device connected to the same room
-        //    and also under the same developer will receive any udp packets this device sends.
-
-        motionDnaSDK.startUDP();
-
-        //    Allow our SDK to record data and use it to enhance our estimation system.
-        //    Send this file to support@navisens.com if you have any issues with the estimation
-        //    that you would like to have us analyze.
-
-        motionDnaSDK.setBinaryFileLoggingEnabled(true);
-
-        //    Tell our SDK how often to provide estimation results. Note that there is a limit on how
-        //    fast our SDK can provide results, but usually setting a slower update rate improves results.
-        //    Setting the rate to 0ms will output estimation results at our maximum rate.
-
-        motionDnaSDK.setCallbackUpdateRateInMs(500);
-
-        //    When setLocationNavisens is enabled and setBackpropagationEnabled is called, once Navisens
-        //    has initialized you will not only get the current position, but also a set of latitude
-        //    longitude coordinates which lead back to the start position (where the SDK/App was started).
-        //    This is useful to determine which building and even where inside a building the
-        //    person started, or where the person exited a vehicle (e.g. the vehicle parking spot or the
-        //    location of a drop-off).
-        motionDnaSDK.setBackpropagationEnabled(true);
-
-        //    If the user wants to see everything that happened before Navisens found an initial
-        //    position, he can adjust the amount of the trajectory to see before the initial
-        //    position was set automatically.
-        motionDnaSDK.setBackpropagationBufferSize(2000);
-
-    //    Enables AR mode. AR mode publishes orientation quaternion at a higher rate.
-
-//        motionDnaSDK.setARModeEnabled(true);
+        motionDnaSDK.start(devKey);
     }
 
     //    This event receives the estimation results using a MotionDna object.
@@ -142,22 +60,22 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
     @Override
     public void receiveMotionDna(MotionDna motionDna)
     {
-        String str = "Navisens MotionDna Location Data:\n";
-        str += motionDnaSDK.checkSDKVersion() + "\n";
-        str += "Lat: " + motionDna.getLocation().globalLocation.latitude + " Lon: " + motionDna.getLocation().globalLocation.longitude + "\n";
-        MotionDna.XYZ location = motionDna.getLocation().localLocation;
+        String str = "Navisens MotionDnaSDK Estimation:\n";
+        str += MotionDnaSDK.SDKVersion() + "\n";
+        str += "Lat: " + motionDna.getLocation().global.latitude + " Lon: " + motionDna.getLocation().global.longitude + "\n";
+        MotionDna.CartesianLocation location = motionDna.getLocation().cartesian;
         str += String.format(" (%.2f, %.2f, %.2f)\n",location.x, location.y, location.z);
-        str += "Hdg: " + motionDna.getLocation().heading +  " \n";
-        str += "motionType: " + motionDna.getMotion().motionType + "\n";
+        str += "Hdg: " + motionDna.getLocation().global.heading +  " \n";
+        str += "motionType: " + motionDna.getClassifiers().get("motion").prediction.label + "\n";
 
         str += "Predictions (BETA): \n\n";
         HashMap<String, MotionDna.Classifier> classifiers =  motionDna.getClassifiers();
         for (Map.Entry<String, MotionDna.Classifier> entry : classifiers.entrySet()) {
             str += String.format("Classifier: %s\n",entry.getKey());
-            str += String.format("\tcurrent prediction: %s confidence: %.2f\n",entry.getValue().currentPredictionLabel, entry.getValue().currentPredictionConfidence);
+            str += String.format("\tcurrent prediction: %s confidence: %.2f\n",entry.getValue().prediction.label, entry.getValue().prediction.confidence);
             str += "\tprediction stats:\n";
 
-            for (Map.Entry<String, MotionDna.PredictionStats> statsEntry : entry.getValue().predictionStatsMap.entrySet()) {
+            for (Map.Entry<String, MotionDna.PredictionStats> statsEntry : entry.getValue().statistics.entrySet()) {
                 str += String.format("\t%s",statsEntry.getKey());
                 str += String.format("\t duration: %.2f\n",statsEntry.getValue().duration);
                 str += String.format("\t distance: %.2f\n",statsEntry.getValue().distance);
@@ -176,98 +94,43 @@ public class MainActivity extends AppCompatActivity implements MotionDnaInterfac
         });
     }
 
-    //    This event receives estimation results from other devices in the server room. In order
-    //    to receive anything, make sure you call startUDP to connect to a room. Again, it provides
-    //    access to a MotionDna object, which can be unpacked the same way as above.
-    //
-    //
-    //    If you aren't receiving anything, then the room may be full, or there may be an error in
-    //    your connection. See the reportError event below for more information.
 
     @Override
-    public void receiveNetworkData(MotionDna motionDna) {
-
-        networkUsers.put(motionDna.getID(),motionDna);
-        double timeSinceBootSeconds = elapsedRealtime() / 1000.0;
-        networkUsersTimestamps.put(motionDna.getID(),timeSinceBootSeconds);
-        StringBuilder activeNetworkUsersStringBuilder = new StringBuilder();
-        List<String> toRemove = new ArrayList();
-
-        activeNetworkUsersStringBuilder.append("Network Shared Devices:\n");
-        for (MotionDna user: networkUsers.values()) {
-            if (timeSinceBootSeconds - networkUsersTimestamps.get(user.getID()) > 2.0) {
-                toRemove.add(user.getID());
-            } else {
-                activeNetworkUsersStringBuilder.append(user.getDeviceName());
-                MotionDna.XYZ location = user.getLocation().localLocation;
-                activeNetworkUsersStringBuilder.append(String.format(" (%.2f, %.2f, %.2f)",location.x, location.y, location.z));
-                activeNetworkUsersStringBuilder.append("\n");
-            }
-
+    public void reportStatus(MotionDnaSDK.Status status, String s) {
+        switch (status) {
+            case AuthenticationFailure:
+                System.out.println("Error: Authentication Failed " + s);
+                break;
+            case AuthenticationSuccess:
+                System.out.println("Status: Authentication Successful " + s);
+                break;
+            case ExpiredSDK:
+                System.out.println("Status: SDK expired " + s);
+                break;
+            case PermissionsFailure:
+                System.out.println("Status: permissions not granted " + s);
+                break;
+            case MissingSensor:
+                System.out.println("Status: sensor missing " + s);
+                break;
+            case SensorTimingIssue:
+                System.out.println("Status: sensor timing " + s);
+                break;
+            case Configuration:
+                System.out.println("Status: configuration " + s);
+                break;
+            case None:
+                System.out.println("Status: None " + s);
+                break;
+            default:
+                System.out.println("Status: Unknown " + s);
         }
-        for (String key: toRemove) {
-            networkUsers.remove(key);
-            networkUsersTimestamps.remove(key);
-        }
-
-        networkTextView.setText(activeNetworkUsersStringBuilder.toString());
-    }
-
-    //    This event receives arbitrary data from the server room. You must have
-    //    called startUDP already to connect to the room.
-
-    @Override
-    public void receiveNetworkData(MotionDna.NetworkCode networkCode, Map<String, ?> map) {
-
-    }
-
-    //    Report any errors of the estimation or internal SDK
-
-    @Override
-    public void reportError(MotionDna.ErrorCode errorCode, String s) {
-        switch (errorCode) {
-            case ERROR_AUTHENTICATION_FAILED:
-                System.out.println("Error: authentication failed " + s);
-                break;
-            case ERROR_SDK_EXPIRED:
-                System.out.println("Error: SDK expired " + s);
-                break;
-            case ERROR_PERMISSIONS:
-                System.out.println("Error: permissions not granted " + s);
-                break;
-            case ERROR_SENSOR_MISSING:
-                System.out.println("Error: sensor missing " + s);
-                break;
-            case ERROR_SENSOR_TIMING:
-                System.out.println("Error: sensor timing " + s);
-                break;
-        }
-    }
-
-    //    The two required methods shown below bind
-    //    the interface to your application's activity,
-    //    so MotionDna is able to retrieve the necessary
-    //    permissions and capabilities
-    @Override
-    public PackageManager getPkgManager() {
-        return getPackageManager();
-    }
-
-    @Override
-    public Context getAppContext() {
-        return getApplicationContext();
     }
 
     protected void onDestroy() {
 
         // Shuts downs the MotionDna Core
         motionDnaSDK.stop();
-
-        // Handle destruction of the foreground service if
-        // it is enabled
-        if (motionDnaServiceIntent != null) {
-            getAppContext().stopService(motionDnaServiceIntent);
-        }
         super.onDestroy();
     }
 }
